f943b83446c2861ffdca6c90ab7f53be7eab9aa4
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-02-23 15:07:30 +0900

    Initial Commit

---
+++ Sample20-LongPress/Sample20_LongPressApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample20_LongPressApp.swift
+//  Sample20-LongPress
+//
+//  Created by keiji yamaki on 2021/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Sample20_LongPressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
